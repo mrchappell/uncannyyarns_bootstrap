a4571eb5ef90a81d9aab31a411e98aedc1998084
mrchappell <michael.r.chappell@gmail.com>
2020-06-06 11:17:31 -0500

    css changes to columns

--- js/scripts.js
+++ js/scripts.js
@@ -19,3 +19,19 @@ $(function () {
         $("#loginModal").modal("show");
       });
 });
+
+
+$('.dropdown-toggle').click(function(e) {
+    if ($(document).width() > 768) {
+      e.preventDefault();
+  
+      var url = $(this).attr('href');
+  
+      if (url !== '#') {
+        window.location.href = url;
+      }
+  
+    }
+  });
+
+  
